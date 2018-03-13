f9e995680f1e366532ce53bc2b4c2321c1d1603b
michael <mlyonsru@gmail.com>
2018-03-13 10:11:31 -0600

    added a list of nouns to be used for alias generation and added functions to add to the patient dictionary

--- clinical_study_organizer/study.py
+++ clinical_study_organizer/study.py
@@ -1,8 +1,22 @@
+import random
 
 
 class Study():
     def __init__(self):
-        pass
+        self.patients = {}
+
+    def add_patients(self, patients):
+        for patient in patients:
+            self.add_patient(patient)
+
+    def add_patient(self, patient):
+        alias = self._ensure_no_duplicate_alias(get_alias())
+        self.patients[alias] = patient
+
+    def _ensure_no_duplicate_alias(self, alias):
+        while self.patients.get(alias, 'empty') != 'empty':
+            alias = get_alias()
+        return alias
 
 
 class Patient():
@@ -13,8 +27,24 @@ class Patient():
         self.data = data
 
 
-def anonymize(data):
-    alias = []
+def get_alias():
+    file = open('../data/nounlist.txt')
+    words = []
+    for line in file:
+        words.append(line)
+
+    first = random.choice(words).replace('\n', '')
+    second = ensure_no_duplicate_noun(first, first, words)
+
+    return first + '_' + second
 
 
+def ensure_no_duplicate_noun(first, second, words):
+    while second == first:
+        second = random.choice(words).replace('\n', '')
+    return second
+
+
+while True:
+    print(get_alias())
 
