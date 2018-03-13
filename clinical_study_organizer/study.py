2f6e5fd99d18e5c2301a4e9614a523f5a090f657
michael <mlyonsru@gmail.com>
2018-03-12 18:54:39 -0600

    added database file

--- clinical_study_organizer/study.py
+++ clinical_study_organizer/study.py
@@ -1,4 +1,23 @@
+import hashlib
+
 class Study():
     def __init__(self):
         pass
 
+def hash_id(identifier):
+
+    return hashlib.md5(identifier.encode())
+
+
+def to_decimal(hex_string):
+    return int(hex_string, 16)
+
+
+
+
+a = hash_id('nerd')
+a = a.hexdigest()
+print('Hex', a)
+b = int(a, 16)
+print('Dec', b)
+print(type(hex(b)))
