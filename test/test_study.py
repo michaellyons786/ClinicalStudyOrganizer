99ac0db58ceef2f8798d5f2c11d4a33e923311d5
michael <mlyonsru@gmail.com>
2018-05-23 08:29:26 -0600

    added test_study skeleton.

--- test/test_study.py
+++ test/test_study.py
@@ -1,4 +1,4 @@
-from clinical_study_organizer.study import read_patient_list, construct_patient_list
+from clinical_study_organizer.study import read_patient_list, construct_patient_list, Study
 
 
 def test_read_patient_list():
@@ -20,3 +20,9 @@ def test_construct_patient_list():
     assert (patients[19].first_name == "Oretha")
     assert (patients[0].id == '7698')
     assert (patients[19].id == '5782')
+
+def test_study():
+    test_list = "../database/sample_patient_list.csv"
+    attributes, data = read_patient_list(test_list)
+
+    study = Study(attributes)
