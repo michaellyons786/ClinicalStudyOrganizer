1597e134b98c9c1c9ddf211ab02187db558e6705
michael <mlyonsru@gmail.com>
2018-05-22 18:37:35 -0600

    added two tests for the study helper functions.

---
+++ test/test_study.py
@@ -0,0 +1,22 @@
+from clinical_study_organizer.study import read_patient_list, construct_patient_list
+
+
+def test_read_patient_list():
+    test_list = "../database/sample_patient_list.csv"
+    attributes, data = read_patient_list(test_list)
+
+    assert("Kolbe" in data[0])
+    assert("Maycock" in data[19])
+    assert("id;INT" in attributes[0])
+    assert("eye_color;VARCHAR" in attributes[5])
+
+def test_construct_patient_list():
+    test_list = "../database/sample_patient_list.csv"
+    attributes, data = read_patient_list(test_list)
+
+    patients = construct_patient_list(data)
+
+    assert (patients[0].first_name == "Cathi")
+    assert (patients[19].first_name == "Oretha")
+    assert (patients[0].id == '7698')
+    assert (patients[19].id == '5782')
