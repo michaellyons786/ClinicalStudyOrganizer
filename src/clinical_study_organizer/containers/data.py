364cd7e832f02ebde2dcfcb8533dd633e8a3764d
michael <mlyonsru@gmail.com>
2018-05-30 18:35:23 -0600

    added data container that handles the lower level parsing.

---
+++ src/clinical_study_organizer/containers/data.py
@@ -0,0 +1,65 @@
+import csv
+
+from containers.patient import Patient
+
+
+class Data():
+    def __init__(self, file_name, noun_list_location):
+        raw_attribute_names, data = read_patient_list(file_name)
+        attribute_names, attribute_types = parse_attribute_names(raw_attribute_names)
+
+        self.attribute_names = attribute_names
+        self.attribute_types = attribute_types
+        self.patients = construct_patient_list(data, noun_list_location)
+
+    def get_attribute_names(self):
+        return self.attribute_names
+
+    def get_attribute_types(self):
+        return self.attribute_types
+
+    def get_patients(self):
+        return self.patients
+
+
+def read_patient_list(file_name):
+    csv_data = []
+
+    with open(file_name) as csvfile:
+        read_csv = csv.reader(csvfile, skipinitialspace=True)
+        for row in read_csv:
+            csv_data.append(row)
+
+    raw_attribute_names = csv_data[0]
+    data = csv_data[1:]
+
+    return raw_attribute_names, data
+
+
+def construct_patient_list(patients, noun_list):
+    patient_list = []
+
+    for row in patients:
+        id = row[0]
+        last_name = row[1]
+        first_name = row[2]
+        data = row[3:]
+        patient = Patient(id, last_name, first_name, data, noun_list)
+        patient_list.append(patient)
+
+    return patient_list
+
+
+def parse_attribute_names(raw_attribute_names):
+    attribute_types = {}
+    attribute_names = []
+
+    for attribute in raw_attribute_names:
+        attribute = attribute.split(';')
+        attribute_name = attribute[0]
+        attribute_names.append(attribute_name)
+        attribute_type = attribute[1]
+
+        attribute_types[attribute_name] = attribute_type
+
+    return attribute_names, attribute_types
--- src/clinical_study_organizer/study.py
+++ src/clinical_study_organizer/study.py
@@ -1,24 +1,25 @@
 import csv
 import pickle
 
-from src.clinical_study_organizer import database as db
-from src.clinical_study_organizer.containers import patient as p
+from containers.data import parse_attribute_names
+from database import Database
 
 
 class Study:
-    def __init__(self, attributes, database_location="../../database/patients.db"):
-        self.attribute_dictionary = self._parse_attributes(attributes)
+    def __init__(self, data, database_location="../../database/patients.db"):
+        self.attribute_types = data.get_attribute_types()
+        self.attribute_names = data.get_attribute_names()
         self.database = None
         self.database_location = database_location
         self.initialized = False
         self.anonymized = True
 
     def initialize(self):
-        self.database = db.Database(self.database_location)
+        self.database = Database(self.database_location)
         if len(self.database.is_initialized()) != 0: # todo fix
             self.delete_tables()
 
-        self.database.initialize(self.attribute_dictionary)
+        self.database.initialize(self.attribute_types)
         self.initialized = True
 
     def add_patients(self, patients):
@@ -54,65 +55,9 @@ class Study:
         return self.database.get_all_attribute_values(attributes)
 
     @staticmethod
-    def _parse_attributes(attributes):
-        attribute_dictionary = {}
-
-        for attribute in attributes:
-            attribute = attribute.split(';')
-            attribute_name = attribute[0]
-            attribute_type = attribute[1]
-
-            attribute_dictionary[attribute_name] = attribute_type
-
-        return attribute_dictionary
-
-    @staticmethod
     def load(file_name):
         return pickle.load(open(file_name, 'rb'))
 
     def save(self, file_name):
         pickle.dump(self, open(file_name, 'wb'))
-
-
-def read_patient_list(file_name):
-    csv_data = []
-
-    with open(file_name) as csvfile:
-        read_csv = csv.reader(csvfile, skipinitialspace=True)
-        for row in read_csv:
-            csv_data.append(row)
-
-    attributes = csv_data[0]
-    data = csv_data[1:]
-
-    return attributes, data
-
-
-def construct_patient_list(patients, noun_list):
-    patient_list = []
-
-    for row in patients:
-        id = row[0]
-        last_name = row[1]
-        first_name = row[2]
-        data = row[3:]
-        patient = p.Patient(id, last_name, first_name, data, noun_list)
-        patient_list.append(patient)
-
-
-    return patient_list
-
-
-if __name__ == "__main__":
-    attributes, data = read_patient_list('../../database/sample_patient_list.csv')
-    study = Study(attributes)
-    study.initialize()
-
-    patients = construct_patient_list(data, "C:\\Users\\Michael\\AnacondaProjects\\ClinicalStudyOrganizer\\test\\test_resources\\nounlist.txt")
-    study.add_patients(patients)
-    info = study.get_all_attribute_values(["age", "eye_color"])
-
-    study.delete_tables()
-
-    # print(mean(info))
 
